da74297b10230cb4b3b5e3a008dec03fe9013c4a
Melanie <melanienmedina@gmail.com>
2022-07-18 01:07:09 -0700

    Frame version 0

---
+++ src/components/Frame.js
@@ -0,0 +1,16 @@
+//will contain 1 frame to be  containing photos of plants for sale
+
+import { Card, CardContent } from '@mui/material'
+// import React from 'react'
+
+const Frame = () => {
+  return (
+    <Card sx={{ maxWidth: 300 }}>
+        <CardContent>
+
+        </CardContent>
+    </Card>
+  )
+}
+
+export default Frame
