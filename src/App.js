f4a84bd34b7ccfe319bb0eae90de22956a95cbd9
Melanie <melanienmedina@gmail.com>
2022-07-18 02:38:41 -0700

    skeleton of single frame with some card typography

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Router, Route, Link } from 'react-router'
 import { Box } from '@mui/material'
 import { styled } from '@mui/system'
 import NavBar from './components/NavBar'
+import Frame from './components/Frame'
 
 const App = () => {
   return (
     <Box>
       <NavBar />
+      <Frame />
     </Box>
   )
 }
--- src/components/Frame.js
+++ src/components/Frame.js
@@ -1,15 +1,60 @@
 //will contain 1 frame to be  containing photos of plants for sale
 
-import { Card, CardContent } from '@mui/material'
+import { Card, CardActions, CardContent, CardMedia, Typography } from '@mui/material'
 // import React from 'react'
+import { styled } from '@mui/system';
+import IconButton from '@mui/material/IconButton';
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
+
+const Container = styled('div')({
+    width: '100vw',
+    border: '1px solid blue',
+    padding: '5vh 5vw',
+})
+
+const CategoryCard = styled(Card)({
+    maxWidth: 300,
+    height: 300,
+    border: '1px solid red',
+})
+
+const CategoryCardMedia = styled(CardMedia)({
+    height: '80%',
+    borderBottom: '1px solid black',
+})
+
+const CategoryCardContent = styled(CardContent)({
+    padding: 0,
+    '& p':{
+        padding: 0,
+    }
+})
+
+const CardTypography = styled(Typography)({
+    padding: 0,
+    width: 100,
+})
 
 const Frame = () => {
   return (
-    <Card sx={{ maxWidth: 300 }}>
-        <CardContent>
+    <Container>
+        <CategoryCard>
+            <CategoryCardMedia />
+            <CategoryCardContent>
+                <CardTypography>
+                    {/* general variable to be changed between Succs, Plants, and Flowers */}
+                    SUCCS
+                    {/* <CardActions> */}
+                        <IconButton>
+                            <ArrowRightAltIcon />
+                        </IconButton>
+                    {/* </CardActions> */}
+                </CardTypography>
+                
+            </CategoryCardContent>
+        </CategoryCard>
+    </Container>
 
-        </CardContent>
-    </Card>
   )
 }
 
